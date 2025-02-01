a2cc1c22f9fadb1fa169797c4dafb89c959fc4b7
Drehinds007 <andrelhinds@icloud.com>
2025-02-01 11:24:12 -0500

    African American Figures App: Updated navbar added colors, hover color, and applied justify-content: space around

---
+++ src/historyData.js
@@ -0,0 +1,216 @@
+const historyData = [
+  {
+    name: 'Harriet Tubman',
+    birthYear: 1822,
+    deathYear: 1913,
+    contribution:
+      'Led enslaved people to freedom through the Underground Railroad.',
+    category: 'Civil Rights',
+    image: 'harriet_tubman.jpg'
+  },
+  {
+    name: 'Frederick Douglass',
+    birthYear: 1818,
+    deathYear: 1895,
+    contribution:
+      'Escaped slavery and became a leading abolitionist, writer, and speaker.',
+    category: 'Civil Rights',
+    image: 'frederick_douglass.jpg'
+  },
+  {
+    name: 'Arthur Ashe',
+    birthYear: 1943,
+    deathYear: 1993,
+    contribution:
+      'First African American to win Wimbledon and an advocate for HIV/AIDS awareness.',
+    category: 'Sports & Activism',
+    image: 'arthur_ashe.jpg'
+  },
+  {
+    name: 'Maya Angelou',
+    birthYear: 1928,
+    deathYear: 2014,
+    contribution:
+      'Poet, author, and civil rights activist known for works like "I Know Why the Caged Bird Sings".',
+    category: 'Literature & Civil Rights',
+    image: 'maya_angelou.jpg'
+  },
+  {
+    name: 'Shirley Chisholm',
+    birthYear: 1924,
+    deathYear: 2005,
+    contribution:
+      'First African American woman elected to Congress and ran for president in 1972.',
+    category: 'Politics',
+    image: 'shirley_chisholm.jpg'
+  },
+  {
+    name: 'James Baldwin',
+    birthYear: 1924,
+    deathYear: 1987,
+    contribution:
+      'Novelist, playwright, and activist who addressed racial, sexual, and class issues.',
+    category: 'Literature & Activism',
+    image: 'james_baldwin.jpg'
+  },
+  {
+    name: 'Muhammad Ali',
+    birthYear: 1942,
+    deathYear: 2016,
+    contribution:
+      'Boxer and social activist, known for his fight for civil rights and his legendary boxing career.',
+    category: 'Sports & Activism',
+    image: 'muhammad_ali.jpg'
+  },
+  {
+    name: 'Malcolm X',
+    birthYear: 1925,
+    deathYear: 1965,
+    contribution:
+      'Leader in the Civil Rights Movement, advocating for black empowerment and social justice.',
+    category: 'Civil Rights',
+    image: 'malcolm_x.jpg'
+  },
+  {
+    name: 'Barack Obama',
+    birthYear: 1961,
+    deathYear: 'Present',
+    contribution:
+      '44th President of the United States, the first African American president.',
+    category: 'Politics',
+    image: 'barack_obama.jpg'
+  },
+  {
+    name: 'Langston Hughes',
+    birthYear: 1902,
+    deathYear: 1967,
+    contribution:
+      'Poet, social activist, and leader of the Harlem Renaissance.',
+    category: 'Literature & Arts',
+    image: 'langston_hughes.jpg'
+  },
+  {
+    name: 'Toni Morrison',
+    birthYear: 1931,
+    deathYear: 2019,
+    contribution:
+      'Novelist, editor, and professor known for works like "Beloved" and "Song of Solomon".',
+    category: 'Literature',
+    image: 'toni_morrison.jpg'
+  },
+  {
+    name: 'Ida B. Wells',
+    birthYear: 1862,
+    deathYear: 1931,
+    contribution:
+      'Journalist, educator, and early leader in the Civil Rights Movement, known for anti-lynching efforts.',
+    category: 'Civil Rights',
+    image: 'ida_b_wells.jpg'
+  },
+  {
+    name: 'John Lewis',
+    birthYear: 1940,
+    deathYear: 2020,
+    contribution:
+      'Civil rights leader and U.S. congressman, known for his role in the March on Selma and advocating for voting rights.',
+    category: 'Civil Rights & Politics',
+    image: 'john_lewis.jpg'
+  },
+  {
+    name: 'W.E.B. Du Bois',
+    birthYear: 1868,
+    deathYear: 1963,
+    contribution:
+      'Sociologist, historian, and civil rights activist, co-founder of the NAACP.',
+    category: 'Civil Rights & Education',
+    image: 'web_du_bois.jpg'
+  },
+  {
+    name: 'Marcus Garvey',
+    birthYear: 1887,
+    deathYear: 1940,
+    contribution:
+      'Leader of the Pan-African movement and founder of the Universal Negro Improvement Association.',
+    category: 'Activism & Pan-Africanism',
+    image: 'marcus_garvey.jpg'
+  },
+  {
+    name: 'Michael Jackson',
+    birthYear: 1958,
+    deathYear: 2009,
+    contribution:
+      'King of Pop, pioneering figure in music, dance, and entertainment.',
+    category: 'Music & Entertainment',
+    image: 'michael_jackson.jpg'
+  },
+  {
+    name: 'Thurgood Marshall',
+    birthYear: 1908,
+    deathYear: 1993,
+    contribution:
+      'First African American Supreme Court Justice and key figure in the legal fight against segregation.',
+    category: 'Law & Civil Rights',
+    image: 'thurgood_marshall.jpg'
+  },
+  {
+    name: 'Bessie Coleman',
+    birthYear: 1892,
+    deathYear: 1926,
+    contribution: 'First African American woman to hold a pilot’s license.',
+    category: 'Aviation',
+    image: 'bessie_coleman.jpg'
+  },
+  {
+    name: 'Martin Luther King Jr.',
+    birthYear: 1929,
+    deathYear: 1968,
+    contribution:
+      'Leader in the Civil Rights Movement, famous for his "I Have a Dream" speech and advocacy for nonviolent protest.',
+    category: 'Civil Rights',
+    image: 'martin_luther_king_jr.jpg'
+  },
+  {
+    name: 'Alvin Ailey',
+    birthYear: 1931,
+    deathYear: 1989,
+    contribution:
+      'Dancer and choreographer, founder of the Alvin Ailey American Dance Theater.',
+    category: 'Dance & Arts',
+    image: 'alvin_ailey.jpg'
+  },
+  {
+    name: 'Matthew Henson',
+    birthYear: 1866,
+    deathYear: 1955,
+    contribution: 'Explorer, one of the first men to reach the North Pole.',
+    category: 'Exploration',
+    image: 'matthew_henson.jpg'
+  },
+  {
+    name: 'Mae Jemison',
+    birthYear: 1956,
+    deathYear: 'Present',
+    contribution: 'First African American woman to travel in space.',
+    category: 'Space & Science',
+    image: 'mae_jemison.jpg'
+  },
+  {
+    name: 'Althea Gibson',
+    birthYear: 1927,
+    deathYear: 2003,
+    contribution:
+      'First African American woman to compete in the U.S. National Championships and Wimbledon.',
+    category: 'Sports',
+    image: 'althea_gibson.jpg'
+  },
+  {
+    name: 'Ruby Bridges',
+    birthYear: 1954,
+    deathYear: 'Present',
+    contribution:
+      'The first African American child to desegregate an elementary school in the South.',
+    category: 'Civil Rights & Education',
+    image: 'ruby_bridges.jpg'
+  }
+]
+export default historyData
