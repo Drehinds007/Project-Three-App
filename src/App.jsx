dc2657244a7f18d21d98372597e3e9ca11b38a79
Drehinds007 <andrelhinds@icloud.com>
2025-02-03 08:07:22 -0500

    African American Figures App: intergrate props for FigureCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import FigureCard from './components/FigureCard'
 
 const App = () => {
   return (
